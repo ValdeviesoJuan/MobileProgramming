e207327998e53b634be558278c8522175c9bc038
Juan Carlos <160289942+ValdeviesoJuan@users.noreply.github.com>
2024-11-23 22:39:35 +0800

    deleted unnecessary files

--- profilePage/App.js
+++ profilePage/App.js
@@ -14,9 +14,9 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginPage} options={{headerTitleAlign: 'center' }}/>
+        <Stack.Screen name="Login" component={LoginPage} options={{headerTitleAlign: 'center', headerShown: true }}/>
         <Stack.Screen name="UserPage" component={UserPage} options={{title: "Profile", headerTitleAlign: 'center' }}/>
-        <Stack.Screen name="RegisterPage" component={RegisterPage} options={{title: "Register", headerTitleAlign: 'center' }}/>
+        <Stack.Screen name="RegisterPage" component={RegisterPage} options={{title: "Register", headerTitleAlign: 'center', headerShown: true }}/>
         <Stack.Screen name="PasswordRecoveryPage" component={PasswordRecoveryPage} options={{title: "Password Recovery", headerTitleAlign: 'center' }}/>
         <Stack.Screen name="PasswordRecoveryPage2" component={PasswordRecoveryPage2} options={{title: "Password Recovery", headerTitleAlign: 'center' }}/>
         <Stack.Screen name="PasswordRecoverySuccessfulPage" component={PasswordRecoverySuccessfulPage} options={{title: "Password Recovery Successful", headerTitleAlign: 'center', headerLeft: () => null,  headerShown: true }}/>
